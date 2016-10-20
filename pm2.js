0184f34496e93f0d710dd83c102a59409dc68b1f
Nemanja Nedeljković <nemanjan00@gmail.com>
2016-10-20 08:11:19 +0200

    Init

---
+++ pm2.js
@@ -0,0 +1,23 @@
+var pm2 = require('pm2');
+
+pm2.connect(function() {
+	pm2.start({
+		script: 'index.js'
+	}, function(err) {
+		if (err) return console.error('Error while launching applications', err.stack || err);
+		console.log('PM2 and application has been succesfully started');
+		
+		// Display logs in standard output 
+		pm2.launchBus(function(err, bus) {
+			console.log('[PM2] Log streaming started');
+
+			bus.on('log:out', function(packet) {
+			 console.log('[App:%s] %s', packet.process.name, packet.data);
+			});
+				
+			bus.on('log:err', function(packet) {
+				console.error('[App:%s][Err] %s', packet.process.name, packet.data);
+			});
+		});
+	});
+});
