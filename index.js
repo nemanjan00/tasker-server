eb3894765ff2bc3494e0847664b6e3d4ef574c38
Nemanja Nedeljković <nemanjan00@gmail.com>
2016-10-20 08:00:58 +0200

    Init

---
+++ index.js
@@ -0,0 +1,78 @@
+// Init express
+
+var express = require('express');
+var app = express();
+
+app.set("port", process.env.PORT || 3000);
+
+// Parse JSON Body
+
+var bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
+// Add websocket support
+
+var expressWs = require('express-ws')(app);
+
+// Subscriptions
+
+var subscriptions = {};
+
+// Gate for client
+
+app.ws('/gate', function(ws, req) {
+	ws.on('message', function(msg) {
+		msg = JSON.parse(msg);
+
+		if(msg.message == "subscribe"){
+			msg.keys.forEach(function(key){
+				if(subscriptions[key] == undefined){
+					subscriptions[key] = [];
+				}
+
+				subscriptions[key].push(ws);
+			});
+		}
+	});
+});
+
+// Gate for Tasker
+
+app.post('/', function (req, res) {
+	var keys = req.body.key.split("|");
+
+	var sentTo = [];
+
+	keys.forEach(function(key){
+		if(subscriptions[key] == undefined){
+			subscriptions[key] = [];
+		}
+
+		subscriptions[key] = subscriptions[key].filter(function (ws){
+			try{
+				if(!sentTo.includes(ws)){
+					ws.send(JSON.stringify(req.body));
+
+					sentTo.push(ws);
+				}
+				else
+				{
+					ws.send(JSON.stringify({action: "_ping"}));
+				}
+
+				return true;
+			} catch(e){
+				return false;
+			}
+		});
+	});
+
+	res.send({status: "ok", sentTo: sentTo.length});
+});
+
+// Listen
+
+app.listen(app.get("port"), function () {
+	console.log('tasker-server is listening on '+app.get("port")+"!");
+});
+
